c8bd346ae237c5ee8c03702b6b36bdc3c5fd1f01
Devesh Shukla <deveshshukla@users.noreply.github.com>
2026-05-05 17:41:42 +0530

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums, target) {
+        return nums.includes(target);
+    }
+}
