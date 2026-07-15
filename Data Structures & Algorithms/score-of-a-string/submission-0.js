6f8fb379dd38285518541c667ba9a7a984c50f21
Devesh Shukla <deveshshukla@users.noreply.github.com>
2026-07-16 01:17:00 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    scoreOfString(s) {
+        let score = 0;
+
+        for (let i=1; i<s.length; i++) {
+            score += Math.abs(s.charCodeAt(i-1) - s.charCodeAt(i));
+        }
+        return score;
+    }
+}
