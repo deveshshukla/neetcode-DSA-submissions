a0ae17052996fe6e444960b42d6d8f124982a04d
Devesh Shukla <deveshshukla@users.noreply.github.com>
2026-08-13 21:48:49 +0530

    Add: monotonic-array - submission-0

---
+++ Data Structures & Algorithms/monotonic-array/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    isMonotonic(nums) {
+        let isIncr = false;
+        let isDecr = false;
+
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) isIncr = true;
+            if (nums[i] < nums[i - 1]) isDecr = true;
+        }
+
+        if (isIncr && isDecr) return false;
+
+        return true;
+    }
+}
